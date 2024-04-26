1dbbd14dd9845f8063c74cbb8258b6ee9f0a9ff3
CARLA LOPEZ <carlalolajeanne@gmail.com>
2024-04-26 11:23:51 +0200

    :sparkles: Filtres projets JS fonctionnels

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,70 @@
-async function travaux() {
-
-const reponse = await fetch('http://localhost:5678/api/works')
-
-const works = await reponse.json()
-
 const projets = document.querySelector(".gallery")
 
+function afficheTravaux(listeTravaux){
+    projets.innerHTML = ""
+    listeTravaux.forEach(work => {
+        const figure = document.createElement("figure")
+        projets.appendChild(figure)
 
-works.forEach(work => {
-    const figure = document.createElement("figure")
-    projets.appendChild(figure)
+        const img = document.createElement("img")
+        figure.appendChild(img)
+        img.src = work.imageUrl
 
-    const img = document.createElement("img")
-    figure.appendChild(img)
-    img.src = work.imageUrl
+        const figcaption = document.createElement("figcaption")
+        figure.appendChild(figcaption)
+        figcaption.innerHTML = work.title
+    });
+}
 
-    const figcaption = document.createElement("figcaption")
-    figure.appendChild(figcaption)
-    figcaption.innerHTML = work.title
-});
+async function travaux() {
 
-const filtreObjet = document.querySelector(".filtre-objets")
-filtreObjet.addEventListener("click", function(){
+    const reponse = await fetch('http://localhost:5678/api/works')
+
+    const works = await reponse.json()
+
+    afficheTravaux(works)
+
+    const filtreObjet = document.querySelector(".filtre-objets")
+    filtreObjet.addEventListener("click", function(){
+        
+        const travauxFiltre = works.filter((work) => work.categoryId == 1 )
+
+        afficheTravaux(travauxFiltre)
+
+        console.log(travauxFiltre)
+    })
     
-    const resultat = works.filter((work) => work.categoryId = 1 )
-})
+    const filtreAppartement = document.querySelector(".filtre-appartements")
+    filtreAppartement.addEventListener("click", function(){
+        
+        const travauxFiltre = works.filter((work) => work.categoryId == 2 )
 
-console.log(resultat)
+        afficheTravaux(travauxFiltre)
+
+        console.log(travauxFiltre)
+    })
+
+    const filtreHotel = document.querySelector(".filtre-hotels")
+    filtreHotel.addEventListener("click", function(){
+        
+        const travauxFiltre = works.filter((work) => work.categoryId == 3 )
+
+        afficheTravaux(travauxFiltre)
+
+        console.log(travauxFiltre)
+    })
+
+    const filtreTous = document.querySelector(".filtre-tous")
+    filtreTous.addEventListener("click", function(){
+        
+        const travauxFiltre = works
+
+        afficheTravaux(travauxFiltre)
+
+        console.log(travauxFiltre)
+    })
+    
+
 }
 
 travaux()
