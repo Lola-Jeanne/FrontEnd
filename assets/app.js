cfb4939e43a1866ccf9455e1cf6c3008c86f2575
CARLA LOPEZ <carlalolajeanne@gmail.com>
2024-07-26 15:03:46 +0200

    :fire: optimisation code

--- assets/app.js
+++ assets/app.js
@@ -1,61 +1,3 @@
-
-// Pour la preview 
-// const imageInput = document.querySelector("#ajouter-img");
-
-// imageInput.addEventListener("change", () => {
-// const reader = new FileReader();
-
-// reader.addEventListener("load", () => {
-//   document.querySelector(".div-img").style.backgroundImage = `url(${reader.result})`;
-// });
-
-// reader.readAsDataURL(imageInput.files[0]);
-// });
-
-// form.addEventListener("submit", async function (e) {
-//     e.preventDefault();
-  
-//     //On les stock dans d'autre variable pour cibler la valeur saisie par l'utilisateur ou le fichier chargé
-//     const ajoutImage = imageElement.files[0];
-//     const ajoutTitle = titleElement.value;
-//     const ajoutCategory = categoryElement.value;
-  
-//     if (!ajoutImage || !ajoutTitle || !ajoutCategory) {
-//       return (msgErreur.style.visibility = "visible");
-//     } else {
-//       msgErreur.style.visibility = "hidden";
-//     }
-  
-//     // Vérifier la taille de l'image
-//   if (ajoutImage.size > 4 * 1024 * 1024) {
-//     alert("La taille de l'image ne doit pas dépasser 4 Mo, saisissez une nouvelle image.");
-//     return;
-//   }
-  
-//   const formData = new FormData(form);
-//   console.log(formData);
-  
-//   const response = await fetch("http://localhost:5678/api/works", {
-//     method: "POST",
-//     headers: {
-//       Accept: "application/json",
-//       Authorization: `Bearer ${window.localStorage.getItem("token")}`,
-//     },
-//     body: formData,
-//   });
-//   const data = await response.json();
-//   if (response.status === 201) {
-//     listeTravaux.push(data);
-//     afficheWorksMini();  // Mettez à jour la liste de travaux dans la modale
-  
-    // Réinitialisez le formulaire pour un nouvel ajout
-//     form.reset();
-//     document.querySelector(".div-img").style.backgroundImage = "";
-//   }
-// });
-
-// Optimisation de code
-
 const modalContent = document.querySelector('#modal .modal-content')
 
 function afficheTravauxModal(works) {
@@ -63,11 +5,9 @@ function afficheTravauxModal(works) {
     works.forEach(work => {
         const figure = document.createElement("figure")
         modalContent.appendChild(figure)
-        // test logo
-        // modalContent.appendChild(figure)
 
         const img = document.createElement("img")
-        // figure.appendChild(img)
+        
         img.src = work.imageUrl
         img.style.width = "80px"
         img.style.height = "105px"
@@ -88,35 +28,30 @@ function afficheTravauxModal(works) {
 
 
 // Pour fermer la modale
-// document.addEventListener('DOMContentLoaded', function() {
+
 const closeModal = document.querySelector('#closeModalIcon')
 const closeModalDeux = document.querySelector('#closeModalIcon-deux')
 const modal = document.querySelector('#modal')
 const modales = document.querySelector('#modal2')
-// const fondModale = document.querySelector('.fond-modale')
-// const modalContent = document.querySelector('#modalContent')
 
 
     closeModal.addEventListener('click', function(e){
         e.preventDefault()
         modal.style.display= 'none'
-        // fondModale.style.display = 'none'
     })
 
     closeModalDeux.addEventListener('click', function(e){
         e.preventDefault()
         modales.style.display= 'none'
         modal.style.display='none'
-        // fondModale.style.display = 'none'
     })
 
 const ajouterPhoto = document.querySelector('.btn-ajout-photo')
 ajouterPhoto.addEventListener('click', function(e) {
     e.preventDefault()
-    // modalContent.style.display = 'none'
     ajouterPhoto.style.display='block'
 })
-// })
+
 // Pour ouvrir la modale 
 
 function ouvrirModalAfficherTraveaux() {
@@ -124,8 +59,6 @@ function ouvrirModalAfficherTraveaux() {
     const modalProjet = document.querySelector('#btn-modifier-projet')
     const modalContent = modal.querySelector('.modal-content')
     const fondModale = document.querySelector('.fond-modale')
-    // const modalContent = document.querySelector('.modal-content')
-    // const ajouterPhoto = document.getElementById('ajouterPhoto')
 
     modal.style.display = 'flex'
     modal.setAttribute('aria-hidden', 'false')
@@ -141,21 +74,21 @@ function ouvrirModalAfficherTraveaux() {
 }
 
 document.addEventListener('DOMContentLoaded', function(){
-    const openModal = document.querySelectorAll('.txt-edition', '.btn-modifier-mes-projets')
+    const openModal = document.querySelector('.txt-edition')
+    const openModalBtnDeux = document.querySelector('.btn-modifier-mes-projets')
 
     const modal = document.querySelector('#modal')
     const modalProjet = document.querySelector('#btn-modifier-projet')
     const modalContent = modal.querySelector('.modal-content')
-    // const ajouterPhoto = document.getElementById('ajouterPhoto')
 
-    openModal.forEach(button => {
-        button.addEventListener('click', function(e){
+    openModal.addEventListener('click', function(e){
             e.preventDefault()
             ouvrirModalAfficherTraveaux()
-            // modal.style.display = 'flex'
-            // modal.setAttribute('aria-hidden', 'false')
-            // modal.setAttribute('aria-modal', 'true')
-        })
+    })
+    
+    openModalBtnDeux.addEventListener('click', function(e){
+        e.preventDefault()
+        ouvrirModalAfficherTraveaux()
     })
     
     modal.addEventListener('click', function(e) {
@@ -180,70 +113,54 @@ document.addEventListener('DOMContentLoaded', function(){
 
     
     // ajout photo
-
     
-        const formAjoutPhoto = document.querySelector('#modal2')
-        const fondModale = document.querySelector('.fond-modale')
-        const btnAjoutPhoto = document.querySelector('.btn-ajout-modal')
-        // const BackgroundModal = document.styleSheets[1];
-
-
-        btnAjoutPhoto.addEventListener('click', function() {
-            fondModale.style.display = 'none'
-            // modalContent.style.backgroundColor = "white"
-            // BackgroundModal.cssRules[0].style.backgroundColor = "white";
-
+    const formAjoutPhoto = document.querySelector('#modal2')
+    const fondModale = document.querySelector('.fond-modale')
+    const btnAjoutPhoto = document.querySelector('.btn-ajout-modal')
+    
+    btnAjoutPhoto.addEventListener('click', function() {
+        fondModale.style.display = 'none'
         })
 
-        // Pour retourner à la modale précédente
-        const btnRetour = document.getElementById('btnRetour')
-            console.log(btnRetour)
-        
-            btnRetour.addEventListener('click', function() {
-            console.log('fonction ok')
-            // masquer la deuxième modale et affichier la première
-            formAjoutPhoto.style.display = 'none'
-            modal.style.display ='flex'
-            fondModale.style.display = 'flex'
-            modalContent.style.display = 'flex'
-            
-            // closeModal.forEach(button =>{
-            //     button.addEventListener('click', function(e){
-            //         e.preventDefault()
-            //         modal.style.display= 'none'
-            //         modales.style.display= 'none'
-            //     })
-            // })
-        })
+    // Pour retourner à la modale précédente
+    const btnRetour = document.getElementById('btnRetour')
+        console.log(btnRetour)
+    
+        btnRetour.addEventListener('click', function() {
+        console.log('fonction ok')
+        // masquer la deuxième modale et affichier la première
+        formAjoutPhoto.style.display = 'none'
+        modal.style.display ='flex'
+        fondModale.style.display = 'flex'
+        modalContent.style.display = 'flex'
+    })
+    
+    document.getElementById('btnAjouterPhoto').addEventListener('click', function(){
+        document.getElementById('imageFile').click()
+    })
+    
+    document.getElementById('imageFile').addEventListener('change', function(event) {
+        const files = event.target.files
+        if (files && files.length > 0) {
+            const file = files[0]
+            const reader = new FileReader()
+            reader.onload = function(e) {
+                const previewImage = document.getElementById('previewImage')
+                const logoImage = document.getElementById('Logoimage')
+                const txtImg = document.getElementById('btnAjouterPhoto')
+                const infoImg = document.getElementById('infoImage')
+                const fondImg = document.getElementById('fondImage')
 
-        document.getElementById('btnAjouterPhoto').addEventListener('click', function(){
-            document.getElementById('imageFile').click()
-        })
-
-        document.getElementById('imageFile').addEventListener('change', function(event) {
-            const files = event.target.files
-            if (files && files.length > 0) {
-                const file = files[0]
-                const reader = new FileReader()
-                reader.onload = function(e) {
-                    const previewImage = document.getElementById('previewImage')
-                    const logoImage = document.getElementById('Logoimage')
-                    const txtImg = document.getElementById('btnAjouterPhoto')
-                    const infoImg = document.getElementById('infoImage')
-                    const fondImg = document.getElementById('fondImage')
-
-                    logoImage.style.display ='none'
-                    txtImg.style.display = 'none'
-                    infoImg.style.display = 'none'
-                    fondImg.style.textAlign = '-webkit-center'
-                    previewImage.src = e.target.result
-                    previewImage.style.display = 'flex'
-                    previewImage.style.width ='129px'
-                    previewImage.style.height ='193px'
-                }
-                reader.readAsDataURL(file)
-                // form.reset();
-                // document.querySelector("#fondImage").style.backgroundImage = "";
+                logoImage.style.display ='none'
+                txtImg.style.display = 'none'
+                infoImg.style.display = 'none'
+                fondImg.style.textAlign = '-webkit-center'
+                previewImage.src = e.target.result
+                previewImage.style.display = 'flex'
+                previewImage.style.width ='129px'
+                previewImage.style.height ='193px'
+            }
+            reader.readAsDataURL(file)
             } else {
                 console.error('Aucune image sélectionnée')
             }
@@ -317,7 +234,7 @@ document.addEventListener('DOMContentLoaded', function(){
                     const projets = document.querySelector(".gallery")
     
                     console.log("Photo ajoutée")
-                    // formAjoutPhoto.reset()
+
                     formAjoutPhoto.style.display= 'none'
                     const data = await Response.json()
                     console.log(data)
@@ -337,11 +254,9 @@ document.addEventListener('DOMContentLoaded', function(){
 
                     const figure2 = document.createElement("figure")
                     modalContent.appendChild(figure2)
-                    // test logo
-                    // modalContent.appendChild(figure)
 
                     const img2 = document.createElement("img")
-                    // figure.appendChild(img)
+            
                     img2.src = data.imageUrl
                     img2.style.width = "80px"
                     img2.style.height = "105px"
@@ -369,27 +284,7 @@ document.addEventListener('DOMContentLoaded', function(){
                 galleryPhoto.closest('figure').remove()
             }
             })
-
-//     formAjoutPhoto.addEventListener('submit', function(e) {
-//         function ajouterPhoto() {
-//             const btnAjoutPhoto = document.querySelector('.btn-ajout-modal')
-    
-            
-//             ajouterPhoto.addEventListener('click', function() {
-//             modalContent.style.display = 'none' //Pour cacher les images 
-//             formAjoutPhoto.style.display= 'block' //Pour afficher le formulaire
-//         })
-//         e.preventDefault()
-//         callApi(`http://localhost:5678/api/works`, 'POST')
-
-//         const titre = document.querySelector('#titre').value 
-//         const image = document.querySelector('#image').files[0]
-//         console.log("Titre:", titre)
-//         console.log("Image:", image)
-//     })
-// })
-
-})
+        })
 
 async function callApi(endpoint, method, body) {
     const Response = await fetch(endpoint, {
@@ -399,7 +294,6 @@ async function callApi(endpoint, method, body) {
           },
         body: body ? JSON.stringify(body) : null
     })
-    // const data = await Response.json()
     return Response
 }
 
